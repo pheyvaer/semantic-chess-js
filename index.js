7584f42307742de465b5908a85c8d42736ffa2f8
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2018-11-08 10:56:24 +0100

    add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * author: Pieter Heyvaert (pheyvaer.heyvaert@ugent.be)
+ * Ghent University - imec - IDLab
+ */
+
+module.exports = {
+  SemanticChess: require('./lib/semanticchess'),
+  Loader: require('./lib/loader')
+};
